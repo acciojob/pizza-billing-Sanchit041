f04a6f2d69db290eb611916c45f8e5385fd4ba93
gupta <guptasanchit041@gmail.com>
2023-01-27 11:33:22 +0530

    hey

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,73 +1,79 @@
 package com.driver;
 
-import java.awt.*;
-import java.util.Stack;
-
 public class Pizza {
 
-    private int countc=-1;
-    private int counta =-1;
-    private int countb=-1;
     private int price;
     private Boolean isVeg;
     private String bill;
+
+    private int cheesePrice;
+    private int toppingPrice;
+    boolean isCheeseAdded;
+    boolean isToppingAdded;
+    boolean isTakeAwayAdded;
+    boolean isBillGenerated;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
-       if(isVeg)
-       {
-           price = 300;
-           System.out.println("Base Price Of The Pizza: 300");
-       }
-       else
-       {
-           price = 400;
-           System.out.println("Base Price Of The Pizza: 400");
-       }
+        this.isCheeseAdded = false;
+        this.isToppingAdded = false;
+        this.isTakeAwayAdded = false;
+        this.isBillGenerated = false;
+        this.bill = "";
+        if(isVeg == true){
+            this.price = 300;
+            this.toppingPrice = 70;
+        }else{
+            this.price = 400;
+            this.toppingPrice = 120;
+        }
+        this.cheesePrice = 80;
+        this.bill = this.bill + "Base Price Of The Pizza: " + this.price;
     }
 
     public int getPrice(){
         return this.price;
     }
 
+//    boolean flag1 = false;
     public void addExtraCheese(){
         // your code goes here
-        if(counta==-1) {
-            price += 80;
-            System.out.println("Extra Cheese Added: 80");
-            counta=0;
+        if(isCheeseAdded == false){
+            this.price+=cheesePrice;
+            isCheeseAdded = true;
         }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(countb==-1) {
-            if (isVeg) {
-                price += 70;
-                System.out.println("Extra Toppings Added: 70");
-            } else {
-                price += 120;
-                System.out.println("Extra Toppings Added: 120");
-            }
-            countb=0;
+        if(isToppingAdded == false){
+            this.price+=toppingPrice;
+            isToppingAdded = true;
         }
     }
 
     public void addTakeaway(){
         // your code goes here
-
-        if(countc==-1) {
-
-            price += 20;
-            System.out.println("Paperbag Added: 20");
-            countc=0;
+        if(isTakeAwayAdded == false){
+            this.price+=20;
+            isTakeAwayAdded = true;
         }
     }
 
     public String getBill(){
         // your code goes here
-        this.bill = "Total Price: "+String.valueOf(price);
-//        System.out.println("Total Price: " +this.bill);
+        if(isBillGenerated == false){
+            if(isCheeseAdded){
+                this.bill+="\nExtra Cheese Added: "+cheesePrice;
+            }
+            if(isToppingAdded){
+                this.bill+="\nExtra Toppings Added: "+toppingPrice;
+            }
+            if(isTakeAwayAdded){
+                this.bill+="\nPaperbag Added: "+20;
+            }
+            this.bill+="\nTotal Price: "+this.price+"\n";
+            isBillGenerated = true;
+        }
         return this.bill;
     }
 }
